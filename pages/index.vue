e019a399bbba4de1caf2594db7e5b956af58737b
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2022-09-23 22:36:43 -0300

    feat: atualizando

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row align="center">
+    <v-col cols="6">
+      <v-select :items="items" label="Selecione um Produto"></v-select>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  data: () => ({
+    items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+  }),
+}
+</script>
